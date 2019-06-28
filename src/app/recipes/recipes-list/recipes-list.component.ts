3d6c762bad9ea1d9adeaf9a46032c2583185cbd9
mfrandes <frandesmyrce@gmail.com>
2019-06-28 19:06:55 +0300

    Adding test content to Recipes Component

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simply a test', 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.bbcgoodfood.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fcategory_retina%2Fpublic%2Frecipe-collections%2Fcollection-image%2F2013%2F05%2Fnext-level-fish-pie.jpg%3Fitok%3DNDSAapTQ&imgrefurl=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcategory%2Fdishes&docid=H-lSPnHKJkIbXM&tbnid=nGHuuBd0wNCkfM%3A&vet=10ahUKEwi0xYfTw4zjAhXHxoUKHWinDGQQMwhTKBEwEQ..i&w=410&h=370&bih=1008&biw=1920&q=recipe%20food&ved=0ahUKEwi0xYfTw4zjAhXHxoUKHWinDGQQMwhTKBEwEQ&iact=mrc&uact=8')
+  ]
   constructor() { }
 
   ngOnInit() {
