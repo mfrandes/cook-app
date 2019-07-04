b72b2002620aa9dc4a33cde44bda268b95c27dcc
mfrandes <frandesmyrce@gmail.com>
2019-07-04 18:04:30 +0300

    imprt FormsModules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header-component';
@@ -23,6 +24,7 @@ import { SoppingEditComponent } from './shopping-list/shopping-edit/shopping-edi
   ],
   imports: [
     BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
