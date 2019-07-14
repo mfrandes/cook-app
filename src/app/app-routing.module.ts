54b6122cb432cff502b3afd19490c46ea332b022
Mircea <frandesmyrce@gmail.com>
2019-07-14 18:47:32 +0300

    Child routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: "/recipes", pathMatch: 'full'},
-    {path: 'recipes', component: RecipesComponent },
-    {path: 'shopping-list', component: ShoppingListComponent}
+    {path: 'recipes', component: RecipesComponent, children: [
+        {path: '', component: RecipeStartComponent}
+    ] },
+    {path: 'shopping-list', component: ShoppingListComponent},
+    {path: ':id', component: RecipesDetailComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { SoppingEditComponent } from './shopping-list/shopping-edit/shopping-edi
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { AppRoutingModule } from './app-routing.module';
     RecipesItemComponent,
     ShoppingListComponent,
     SoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+    RecipeStartComponent
   ],
   imports: [
     BrowserModule,
