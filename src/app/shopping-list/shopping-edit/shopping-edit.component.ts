bb51136989dc883b6fd59d1634a48af4b23bc009
mfrandes <frandesmyrce@gmail.com>
2019-07-17 18:47:15 +0300

    TD: Adding the Shopping List Form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 import { Ingredient} from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,17 +9,15 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class SoppingEditComponent implements OnInit {
-  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+  
   
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
-  onAddItem() {
-    const ingName: string = this.nameInputRef.nativeElement.value;
-    const amountName: number = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, amountName);
+  onAddItem(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
   }
 }
