6777402de3872e5104c17f692d88f612ea3740a9
Mircea <frandesmyrce@gmail.com>
2019-07-21 16:05:40 +0300

    Sending the Signup Request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,17 +10,43 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent implements OnInit {
   isLogIn = true;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  onSwitchMode(){
+  onSwitchMode() {
     this.isLogIn = !this.isLogIn;
   }
-  onSubmit(form: NgForm){
-    console.log(form.value);
-    form.reset;
-    
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLogIn) {
+      //...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        resData => {
+          console.log(resData);
+        }, error => {
+          console.log(error);
+        }
+      )
+    }
+    form.reset();
+
   }
 }
+
+/*
+Time to fire the sign up,
+we need to extract the email and password each in a const, and allsow we can add if logic to disable the submit if the form is invalid
+now we need to inject the auth.service and than call the signgup() and set email and password as param.
+after taht we need to sbscribe to the response lets log in console the data and the error
+we should do sumething about the is loging we will check if we are logging (here we don"t do enithing now) or else were we will send http request
+we allwais want to reset the form after the submit
+ */
